c9e2d0cf6e6891cf98dc2a1bdac5ede304252b2c
logiclogue <jordanpaullord@hotmail.co.uk>
2017-12-13 16:11:21 +0000

    Adds foreground and background functions for character map2

--- src/char_map.c
+++ src/char_map.c
@@ -5,12 +5,26 @@ CharMap char_map_new(Word start_address, int width, int height) {
     return (CharMap){ start_address, width, height };
 }
 
-char char_map_get_char(CharMap self, Memory memory, int i) {
-    return memory_get(memory, self.start_address + i);
+Word char_map_get_word(CharMap self, Memory memory, int x, int y) {
+    return memory_get(memory, self.start_address + x + (y * self.width));
 }
 
-char char_map_get_char_by_coords(CharMap self, Memory memory, int x, int y) {
-    return memory_get(memory, self.start_address + x + (y * self.width));
+char char_map_get_char(CharMap self, Memory memory, int x, int y) {
+    Word word = char_map_get_word(self, memory, x, y);
+
+    return word & 0xFF;
+}
+
+int char_map_get_foreground(CharMap self, Memory memory, int x, int y) {
+    Word word = char_map_get_word(self, memory, x, y);
+
+    return (word & 0xF000) >> 12;
+}
+
+int char_map_get_background(CharMap self, Memory memory, int x, int y) {
+    Word word = char_map_get_word(self, memory, x, y);
+
+    return (word & 0x0F00) >> 8;
 }
 
 void char_map_test(void) {
@@ -19,10 +33,11 @@ void char_map_test(void) {
 
     memory_set(memory, 0x3000, (Word)'J');
     memory_set(memory, 0x3001, (Word)'o');
-    memory_set(memory, 0x3010, (Word)'r');
+    memory_set(memory, 0x3010, 0x1200 + (Word)'r');
 
-    assert(char_map_get_char(char_map, memory, 0) == (Word)'J');
-    assert(char_map_get_char(char_map, memory, 1) == (Word)'o');
-    assert(char_map_get_char_by_coords(char_map, memory, 0, 0) == (Word)'J');
-    assert(char_map_get_char_by_coords(char_map, memory, 0, 1) == (Word)'r');
+    assert(char_map_get_word(char_map, memory, 0, 0) == 'J');
+    assert(char_map_get_word(char_map, memory, 0, 1) == 0x1200 + 'r');
+    assert(char_map_get_char(char_map, memory, 0, 1) == 'r');
+    assert(char_map_get_foreground(char_map, memory, 0, 1) == 1);
+    assert(char_map_get_background(char_map, memory, 0, 1) == 2);
 }
--- src/char_map.h
+++ src/char_map.h
@@ -13,8 +13,10 @@ struct CharMapTag {
 };
 
 CharMap char_map_new(Word start_address, int width, int height);
-char char_map_get_char(CharMap self, Memory memory, int i);
-char char_map_get_char_by_coords(CharMap self, Memory memory, int x, int y);
+Word char_map_get_word(CharMap self, Memory memory, int x, int y);
+char char_map_get_char(CharMap self, Memory memory, int x, int y);
+int char_map_get_nyble_a(CharMap self, Memory memory, int x, int y);
+int char_map_get_nyble_b(CharMap self, Memory memory, int x, int y);
 void char_map_test(void);
 
 #endif
